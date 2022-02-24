6bfe456aaec5ebcf9d9872028e1f461086e279c9
Yash Garg <yash_fw10_103@masai.school>
2022-02-24 19:56:59 +0530

    creating userSchema & model

---
+++ server/models/user.model.js
@@ -0,0 +1,7 @@
+const mongoose=require("mongoose")
+const UserSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true}
+})
+const User=mongoose.model("/users",UserSchema)
+module.exports=User;
