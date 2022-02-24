2e5add9410f094aaaccdf20eff8bdab2ff881fb8
Yash Garg <yash_fw10_103@masai.school>
2022-02-24 11:06:20 +0530

    working on signup page

---
+++ client/valuenable/src/components/Signup.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export const Signup=()=>{
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState("")
+    const [signUp,setSignUp]=useState([])
+    const handleName=(e)=>{
+        console.log(e.target.value)
+        setName(e.target.value)
+    }
+    const handleEmail=(e)=>{
+        console.log(e.target.value)
+        setEmail(e.target.value)
+    }
+    const handleSignUp=()=>{
+        setSignUp([...signUp,name,email])
+        setName("")
+        setEmail('')
+        
+    }
+    return (
+        <div>
+            <h1>SignUp</h1>
+            <input type="text" placeholder="Your Name" value={name} onChange={handleName} />
+            <input type="email" placeholder="Your Email" value={email} onChange={handleEmail}/>
+            <button onClick={handleSignUp}>SignUP</button>
+            {signUp.map((e,id)=>{return(
+                <div key={id}>{e}</div>
+            )})}
+        </div>
+    )
+}
